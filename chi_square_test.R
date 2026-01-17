7c67f9500acd321d11a52a549d24be492ddee173
sanjit-beep <sanjitmvlu@gmail.com>
2026-01-17 22:35:19 +0530

    Add chi-square test program

--- chi_square_test.R
+++ chi_square_test.R
@@ -1,4 +1,10 @@
+# Chi-Square Test using chisq.test()
 
+# H0 (Null Hypothesis):
+# Gender and course preference are independent.
+
+# H1 (Alternative Hypothesis):
+# Gender and course preference are not independent.
 
 gender <- c("Male", "Male", "Female", "Female", "Male", "Female", "Male", "Female")
 preference <- c("Science", "Commerce", "Science", "Arts",
@@ -6,6 +12,9 @@ preference <- c("Science", "Commerce", "Science", "Arts",
 
 data <- table(gender, preference)
 
-
 chi_result <- chisq.test(data)
 chi_result
+
+# Decision Rule:
+# If p-value < 0.05, reject H0; otherwise accept H0.
+
